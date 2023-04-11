a182ba8c3decc032c4b7afd1690149fb58789045
GabryelJackson <gabryeljackson5@gmail.com>
2023-04-11 00:02:19 -0300

    comentario de codigo

---
+++ aula03/index.js
@@ -0,0 +1,8 @@
+console.log('Gabryel Jackson'); // String = Texto
+console.log("Sacramento Linhares");
+console.log(`Cláudia Carvalho`);
+console.log("'Sacramento'");
+console.log('"Jackson"');
+console.log(`'Barbosa' "Linhares"`);
+console.log(35, 15.85); // Number = Número
+console.log(35, 15.85, 'Luiz Otávio');
