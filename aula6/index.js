5b24d0863c798ce9e4def081c101e128cf9a96f6
GabryelJackson <gabryeljackson5@gmail.com>
2023-04-13 01:00:18 -0300

    aprendendo sobre o let

---
+++ aula6/index.js
@@ -0,0 +1,11 @@
+// não podemos criar variáveis com palavras reservadas.
+// Variáveis precisam ter nomes significativos.
+// Não pode começar o nome de uma variável com um número.
+// Não pode conter espaços ou traços.
+// Utilizamos camelCase (inicia com letra minúscula e depois se tiver outro nome com letra maiúscula).
+// Case-sensitive (leta maiúscula e minúscula faz diferença).
+// não podemos redeclarar variáveis com let.
+// Não utilize var, utilize let.
+let nomeCliente = 'Luiz'
+let nomecliente = 'Otávio'
+console.log(nomeCliente, nomecliente)
