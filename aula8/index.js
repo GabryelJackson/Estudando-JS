1cf2ce6148fb0d541e11e1ee3174e44b6c6d9887
GabryelJackson <gabryeljackson5@gmail.com>
2023-04-14 17:44:49 -0300

    Update index.js

--- aula8/index.js
+++ aula8/index.js
@@ -12,4 +12,4 @@ let imc = peso/(altura*altura); // peso / (altura * altura)
 let anoNascimento = 2023 - idade
 
 console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso}Kg, tem altura de ${altura} e seu IMC é de ${imc}`);
-console.log(`${nome} ${sobrenome} nasceu em ${anoNascimento}`)
+console.log(`${nome} ${sobrenome} nasceu em ${anoNascimento}`);
