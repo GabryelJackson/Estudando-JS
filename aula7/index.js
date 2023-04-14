7c76ce8adbde0addd1dbfe85271c47f8ab81b76d
GabryelJackson <gabryeljackson5@gmail.com>
2023-04-14 00:02:47 -0300

    constantes

---
+++ aula7/index.js
@@ -0,0 +1,19 @@
+// não podemos criar constantes com palavras reservadas.
+// Constantes precisam ter nomes significativos.
+// Não pode começar o nome de uma constante com um número.
+// Não pode conter espaços ou traços.
+// Utilizamos camelCase (inicia com letra minúscula e depois se tiver outro nome com letra maiúscula).
+// Case-sensitive (leta maiúscula e minúscula faz diferença).
+// não podemos modificar o valor de uma constante.
+// Não utilize var, utilize const.
+
+// + - * / - String = texto | Number = Número
+const primeiroNumero = 5 // 
+const segundoNumero = 10
+const resultado = primeiroNumero * segundoNumero
+const resultadoDulpicado = resultado * 2
+let resultadoTriplicado = resultado * 3
+resultadoTriplicado = resultadoTriplicado + 5
+console.log(resultado)
+
+console.log(typeof(resultado))
