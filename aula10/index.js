6824167e8e6a3da0e7d5812d24aadd907012f6e1
GabryelJackson <gabryeljackson5@gmail.com>
2023-04-18 21:54:54 -0300

    tipos de dados primitivos

---
+++ aula10/index.js
@@ -0,0 +1,12 @@
+// String. number, undefined, null, boolean
+
+const nome = 'Gabryel' // String
+const sobrenome = "Jacksoon" // String
+const sobrenome1 = `Sacramento` // String
+const numero = 10 // number
+const numero2 = 10.52 // number
+let nomeAluno; // undefined = não aponta pra local nenhum da memória
+let sobrenomeAluno = null; // Nulo -> Não aponta para local nenhum na memória
+const aprovado = false // Boolean = true, false (lógico)
+
+console.log(typeof aprovado, aprovado)
