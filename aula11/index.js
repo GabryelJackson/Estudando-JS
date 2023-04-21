4d023a44bb5c31ca813905ec1e24b33cb177d15f
GabryelJackson <gabryeljackson5@gmail.com>
2023-04-20 23:21:01 -0300

    Operadores aritimeticos, ordem de precedencia, incremento, operadores de atribuicao

---
+++ aula11/index.js
@@ -0,0 +1,37 @@
+/* 
+    Aritiméticos  
+    + Adição e Concatenação  
+    - Subtração 
+    / Divisão
+    * Multiplicação
+    ** Potenciação
+    % Resto da Divisão
+
+    - Ordem de Precedência
+    () Parênteses
+    ** Potenciação
+    * Multiplicação
+    / Divisão
+    % Módulo da Divisão
+    + Soma
+    - Subtração
+
+    Incremento = ++
+    Decremento = --
+
+    - Operadores de atribuição - 
+    +=
+    *=
+    **=
+
+    NaN - Not a Number
+    parseInt - Converte uma string para um número inteiro
+    parseFloat - Converte uma string para um número com casas decimais
+
+*/
+const num1 = 10
+const num2 = Number('5.3')
+console.log(num1 + num2)
+console.log(typeof num2)
+
+
