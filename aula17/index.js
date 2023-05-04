c665860570ab6e316a748e6e544fe04c31a997d3
GabryelJackson <gabryeljackson5@gmail.com>
2023-05-04 00:39:49 -0300

    exercicios com numbers e math

---
+++ aula17/index.js
@@ -0,0 +1,12 @@
+let numero = Number(prompt('Digite um número'));
+let numeroTitulo = document.getElementById('numero-titulo')
+let texto = document.getElementById('texto')
+let raizQuadrada = document.getElementById('raiz-quadrada')
+
+numeroTitulo.innerHTML = numero
+raizQuadrada.innerHTML = `<strong>${numero ** 0.5}</strong>`
+texto.innerHTML += `<p><strong>${numero}</strong> é inteiro? <strong>${Number.isInteger(numero)}</strong></p>`
+texto.innerHTML += `<p> É NaN? <strong>${isNaN(numero)}</strong></p>`
+texto.innerHTML += `<p> Arredondando para baixo: <strong>${Math.floor(numero)}</strong></p>`
+texto.innerHTML += `<p> Arredondando para cima: <strong>${Math.ceil(numero)}</strong></p>`
+texto.innerHTML += `<p> Com duas casas decimais <strong>${numero.toFixed(2)}</strong></p>`
