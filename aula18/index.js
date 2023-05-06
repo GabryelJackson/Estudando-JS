e740926ab847df778a5f3dc3694ab819b7bb94c4
GabryelJackson <gabryeljackson5@gmail.com>
2023-05-06 19:35:36 -0300

    Arrays (Basico)

---
+++ aula18/index.js
@@ -0,0 +1,40 @@
+//          0123456789
+let nome = 'Luiz Otávio'
+//              0       1         2
+let alunos = ['Luiz', 'Maria', 'João'] // Luiza, Eduardo
+alunos = 123
+console.log(typeof alunos)
+console.log(alunos instanceof Array)
+
+// alunos.push('Luiza')
+// alunos.push('Eduardo')
+// console.log(alunos.slice(0, -2))
+
+// console.log(alunos[50])
+
+// delete alunos [1]
+// console.log(alunos[1])
+
+// let removido = alunos.shift()
+// console.log(removido)
+// console.log(alunos)
+
+
+// alunos.unshift('Luisa') // Adiciona no começo
+// alunos.unshift('Fábio')
+
+// alunos.push('Luisa') - Adiciona noo Fim
+// alunos.push('Fabio')
+
+
+// alunos[alunos.length] = 'Luisa' - Adiciona no Fim 
+// alunos[alunos.length] = 'Fabio'
+// alunos[alunos.length] = 'Luana'
+
+// alunos[0] = 'Eduardo' - Altera
+// alunos[3] = 'Luiza'
+
+
+// console.log(alunos)
+// console.log(alunos[0]);
+// console.log(alunos[2])
