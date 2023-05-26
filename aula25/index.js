3ff21b0df5cfc48eb26cdad58ff88d4680d333d4
GabryelJackson <gabryeljackson5@gmail.com>
2023-05-26 00:04:04 -0300

    avaliação de curto-circuito (short-circuit)

---
+++ aula25/index.js
@@ -0,0 +1,33 @@
+/* 
+    && - AND - 
+    || - OR -
+
+    FALSY
+    false
+    0
+    '', "", ``
+    null / undefined
+    NaN
+*/ 
+
+// function falaOi() {
+//     return 'Oi';
+// }
+
+// let vaiExecutar = 'Joãozinho'
+
+// console.log(vaiExecutar && falaOi());
+
+// console.log(0 || false || null || 'Gabryel' || true);
+
+// let corUsuario = 'vermelho';
+// let corPadrao = corUsuario || 'preto';
+// console.log(corPadrao);
+
+const a = 0
+const b = null
+const c = 'false'
+const d = false
+const e = NaN
+
+console.log(a || b || c || d || e)
