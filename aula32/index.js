addb33419be51c0973b2caeed422d2028fecae33
GabryelJackson <gabryeljackson5@gmail.com>
2023-06-22 22:17:22 -0300

    diferença entre var/let/const

---
+++ aula32/index.js
@@ -0,0 +1,36 @@
+const verdadeira = true;
+
+// Let tem escopo de bloco {... bloco}
+// Var só tem escopo de função
+
+// let nome = 'Luiz'; // criando
+// var nome2 = 'Luiz'; // criando
+
+// if(verdadeira) {
+//     let nome = 'Otávio'; // criando
+//     var nome2 = 'Rogério';
+    
+
+//     if(verdadeira) {
+//         var nome2 = 'Ronaldo';
+//         let nome = 'Outra coisa';
+//     }
+// }
+
+// console.log(nome, nome2);
+
+
+// function falaOi() {
+    
+//     if(verdadeira) {
+//         let nome = 'Gabryel';
+//         var sobrenome = 'Jackson';
+//     }
+
+//     console.log(nome, sobrenome);
+// }
+
+// falaOi();
+
+let sobrenome = 'Sacramento';
+console.log(sobrenome);
