d3bc8d8c31b3e45177714bf50a7f87b3e19d5a1c
GabryelJackson <gabryeljackson5@gmail.com>
2023-07-04 23:08:29 -0300

    for of - estrutura de repetição

---
+++ aula38/index.js
@@ -0,0 +1,38 @@
+const pessoas = {
+    nome: 'Gabryel',
+    sobrenome: 'Jackson'
+};
+
+// for clássico  - geralmente com iteeráveis (array ou strings);
+// for in - retorna o índice ou chave (string, array ou objetos);
+// for of - retorna o valor em si (iteráveis, arrays ou strings);
+
+
+// for(let chave in pessoas) {
+//     console.log(chave, pessoas[chave]);
+// }
+
+// const nome = ['Gabryel', 'Jackson', 'Samuel'];
+
+// for(let valor of nome) {
+//     console.log(valor);
+// };
+
+// console.log('########');
+
+// nome.forEach(function(valor, indice, array) {
+//     console.log(valor, indice, array);
+// });
+
+// console.log('########');
+
+// for(let i in nome) {
+//     console.log(nome[i]);
+// };
+
+// console.log('########');
+
+// for(let i = 0; i < nome.length; i++) {
+//     console.log(nome[i]);
+// };
+
