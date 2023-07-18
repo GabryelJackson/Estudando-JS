d8999431a9327100af640689309885ab34b49838
GabryelJackson <gabryeljackson5@gmail.com>
2023-07-17 21:40:55 -0300

    tratando e lancando erros (try, catch, finally)

---
+++ aula46/index.js
@@ -0,0 +1,36 @@
+// try {
+//     // Executada quando não há erros
+//     console.log(a);
+//     console.log('Abri um arquivo!');
+//     console.log('Manipulei o arquivo!');
+//     console.log('Fechei o arquivo!');
+// } catch(e) {
+//     // Quando há erros
+//     console.log('Tratando o erro!');
+// } finally {
+//     // Sempre
+//     console.log('FINALLY: Eu sempre sou executado');
+// }
+
+function retornaHora(data) {
+    if(data && !(data instanceof Date)) {
+        throw new TypeError('Esperando instância de Date.');
+    }
+
+    if(!data) {
+        data = new Date();
+    }
+
+    return data.toLocaleTimeString('pt-BR', {hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit'});
+}
+
+try {
+    const data = new Date('01-01-1970 12:58:12');
+    const hora = retornaHora();
+    console.log(hora);
+} catch (e) {
+    // Tratar erro
+    console.log(e);
+} finally {
+    console.log('Tenha um bom dia!');
+}
