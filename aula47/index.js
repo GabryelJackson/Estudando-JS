63afcc82eb5c6535ca4a4ee73585b5cf4ea2eded
GabryelJackson <gabryeljackson5@gmail.com>
2023-07-18 22:36:56 -0300

    setInterval e setTimeout

---
+++ aula47/index.js
@@ -0,0 +1,17 @@
+function mostraHora() {
+    const data = new Date();
+
+    return data.toLocaleTimeString('pt-BR', {hour12: false});
+}
+
+const timer = setInterval(function () {
+    console.log(mostraHora());
+}, 1000);
+
+setTimeout(function() {
+    clearInterval(timer)
+}, 10000);
+
+setTimeout(function() {
+    console.log('Olá Mundo!')
+}, 5000);
