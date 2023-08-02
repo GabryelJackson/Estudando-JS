a3d3bda312db6da29a0fa4a751453e64097bdbde
GabryelJackson <gabryeljackson5@gmail.com>
2023-08-02 17:40:24 -0300

    parametros da funcao

---
+++ aula51/index.js
@@ -0,0 +1,16 @@
+// arguments que sustenta todos os argumentos enviados
+const conta = function (operador, acumulador, ...numeros) {
+    for(let numero of numeros) {
+        if(operador === '+') {
+            acumulador += numero;
+        }else if(operador === '-') {
+            acumulador -= numero;
+        }else if(operador === '*') {
+            acumulador *= numero;
+        }else if(operador === '/') {
+            acumulador /= numero;
+        }
+    }
+    console.log(acumulador);
+};
+conta('+', 1, 20, 30, 40, 50);
