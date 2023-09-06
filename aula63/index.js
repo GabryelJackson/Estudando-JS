8336d078a2b232cf05c7146b628d3302ea9b1cc9
GabryelJackson <gabryeljackson5@gmail.com>
2023-09-05 23:17:35 -0300

    revisao do basico em arrays

---
+++ aula63/index.js
@@ -0,0 +1,9 @@
+// Valor por referência
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana'];
+// const novo = nomes.slice(1, -1);
+// console.log(novo);
+
+// const nome = 'Gabryel Jackson Sacramento Linhares';
+const nomes = ['Gabryel', 'Jackson', 'Sacramento', 'Linhares'];
+const nome = nomes.join(' ');
+console.log(nome);
