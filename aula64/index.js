8b4e38402a9f4f678de706915c80b8d9cd7c6f03
GabryelJackson <gabryeljackson5@gmail.com>
2023-09-07 00:03:14 -0300

    metodo splice

---
+++ aula64/index.js
@@ -0,0 +1,9 @@
+const nomes = ['Martia', 'João', 'Eduardo', 'Gabryel', 'Júlia'];
+// nomes.splice(indice, delete, elem2, elem3);
+// pop
+// const removidos = nomes.splice(-1, 1);
+// shift
+// const removidos = nomes.splice(-5, 1);
+// push
+nomes.splice(0, 0, 'Luiz', 'Otávio');
+console.log(nomes);
