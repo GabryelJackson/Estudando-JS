ff879f720fa36876390e17c7330bfa1f90898ef5
GabryelJackson <gabryeljackson5@gmail.com>
2023-09-11 23:32:09 -0300

    Concatenando arrays

---
+++ aula65/index.js
@@ -0,0 +1,5 @@
+const numeros = [1, 2, 3];
+const numeros2 = [4, 5, 6];
+const a3 = [...numeros, 'Gabryel', ...numeros2, ...[7, 8, 9]]
+// const array3 = numeros.concat(numeros2, [7, 8, 9], 'Gabryel');
+console.log(a3);
