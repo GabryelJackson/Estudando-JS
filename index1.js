18c7c5a27d7575d3765c4dd8a8160c6c0bac86e8
GabryelJackson <gabryeljackson5@gmail.com>
2024-03-13 17:22:37 -0300

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,32 +1,31 @@
-// const tresHoras = 60 * 60 * 3 * 1000;
-// const umDia = 60 * 60 * 24 * 1000;
-// const data = new Date(0 + tresHoras + umDia);
-// const data = new Date();
-// console.log(data.getDate());
-// console.log(data.getMonth()+ 1);
-// console.log(data.getFullYear());
-// console.log(data.getHours());
-// console.log(data.getMinutes());
-// console.log(data.getSeconds());
-// console.log(data.getMilliseconds());
-// console.log(data.getDay());
+const data = new Date();
+const diaDaSemana = data.getDay();
+let diaDaSemanaTexto;
 
-function zeroAEsquerda(num) {
-    return num <= 10 ? `0${num}` : num;
-} 
-
-function formataData(data) {
-    const dia = zeroAEsquerda(data.getDate());
-    const mes = zeroAEsquerda(data.getMonth() + 1);
-    const ano = zeroAEsquerda(data.getFullYear());
-    const hora = zeroAEsquerda(data.getHours());
-    const minuto = zeroAEsquerda(data.getMinutes());
-    const segundo = zeroAEsquerda(data.getSeconds());
-
-    return `${dia}/${mes}/${ano} ${hora}:${minuto}:${segundo}`;
+switch (diaDaSemana) {
+    case 0:
+        diaDaSemanaTexto = 'Domingo';
+        break;
+    case 1:
+        diaDaSemanaTexto = 'Segunda';
+        break;
+    case 2:
+        diaDaSemanaTexto = 'Terça';
+        break;
+    case 3:
+        diaDaSemanaTexto = 'Quarta';
+        break;
+    case 4:
+        diaDaSemanaTexto = 'Quinta';
+        break;
+    case 5:
+        diaDaSemanaTexto = 'Sexta';
+        break;
+    case 6:
+        diaDaSemanaTexto = 'Sábado';
+        break;
 }
 
-const data = new Date();
-const dataBrasil = formataData(data);
-console.log(dataBrasil);
+console.log(diaDaSemana, diaDaSemanaTexto);
+
 
