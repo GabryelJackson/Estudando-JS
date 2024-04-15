84dee96b9eb0b9a53897d2d26679855fa9ee574b
GabryelJackson <gabryeljackson5@gmail.com>
2024-04-14 21:23:51 -0300

    atualizacao

--- aula39/assets/js/index.js
+++ aula39/assets/js/index.js
@@ -1,16 +1,11 @@
 const paragrafos = document.querySelector('.paragrafos');
-console.log(paragrafos);
 const ps = paragrafos.querySelectorAll('p');
-console.log(ps);
 
-const estilosBody = getComputedStyle(document.body);
-const backgroundColorBody = estilosBody.backgroundColor;
-const corDaCaixa = '#e0daa3';
-
-console.log(backgroundColorBody);
+const styleBody = getComputedStyle(document.body);
+const background = styleBody.backgroundColor;
+console.log(background);
 
 for(let p of ps) {
-    p.style.backgroundColor = backgroundColorBody;
-    p.style.color = corDaCaixa;
-    // console.log(p);
+    p.style.backgroundColor = background;
+    p.style.color = '#e0daa3';
 }
