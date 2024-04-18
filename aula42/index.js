1eec3ea7b7a8bede7a105365d530926d4dd0411c
GabryelJackson <gabryeljackson5@gmail.com>
2024-04-18 16:36:47 -0300

    Update index.js

--- aula42/index.js
+++ aula42/index.js
@@ -1,14 +1,13 @@
 //  Escreva uma função que recebe 2 números e retorne o maior deles
 
-function maiorNumero() {
-    let num1 = 2;
-    let num2 = 4;
-
-    if(num1 > num2) {
-        console.log(`${num1} é maior que ${num2}`);
-    } else if(num2 > num1) {
-        console.log(`${num2} é maior que ${num1}`);
+function twoNumbers(x, y) {
+    if(x > y) {
+        return x;
+    } else if (y > x) {
+        return y;
     }
 }
+// Trabalho de preguiçoso
+const function2 = (x, y) => x > y ? x : y;
+console.log(function2(54, 423));
 
-maiorNumero();
