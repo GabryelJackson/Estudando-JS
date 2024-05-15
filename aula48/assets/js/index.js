50610a8199dc0d83cdb4456c120869d3b5c5e5aa
GabryelJackson <gabryeljackson5@gmail.com>
2024-05-15 00:21:57 -0300

    refazendo o cronometro

--- aula48/assets/js/index.js
+++ aula48/assets/js/index.js
@@ -1,43 +1,20 @@
-function relogio() {
-    function pegarHoraDosSegundos(segundos) {
-        const data = new Date(segundos * 1000);
-        return data.toLocaleTimeString('pt-BR', {hour12: false, timeZone: 'GMT'});
-    }
-    
-    console.log(pegarHoraDosSegundos(10));
-    
-    const relogio = document.querySelector('.relogio');
-    let segundos = 0;
-    let timer;
-    
-    function iniciarRelogio() {
-        timer = setInterval(function() {
-            segundos++;
-            relogio.innerHTML = pegarHoraDosSegundos(segundos)
-        }, 1000);
-    }
-    
-    document.addEventListener('click', function(e) {
-        const elemento = e.target;
-        console.log(elemento);
-    
-        if(elemento.classList.contains('iniciar')) {
-            relogio.classList.remove('pausado');
-            clearInterval(timer);
-            iniciarRelogio();
-        };
-    
-        if(elemento.classList.contains('pausar')) {
-            clearInterval(timer);
-            relogio.classList.add('pausado');
-        };
-    
-        if(elemento.classList.contains('zerar')) {
-            relogio.classList.remove('pausado');
-            clearInterval(timer);
-            relogio.innerHTML = '00:00:00';
-            segundos = 0;
-        };
-    });
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+let segundos = 0;
+
+function cronometro() {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {hour12: false, timeZone: 'GMT'});
+
 }
-relogio();
+
+iniciar.addEventListener('click', function iniciarCronometro() {
+    const timer = setInterval(function() {
+        segundos++;
+        relogio.innerHTML = cronometro(timer);
+    }, 1000)
+});
+
+
