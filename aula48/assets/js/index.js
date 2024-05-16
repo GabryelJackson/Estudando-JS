cbfe629c43f6299c1514dd18aaa79cf088c7c16f
GabryelJackson <gabryeljackson5@gmail.com>
2024-05-15 21:42:29 -0300

    refiz o cronometro solo kkk mt orgulhoso de mim

--- aula48/assets/js/index.js
+++ aula48/assets/js/index.js
@@ -3,18 +3,30 @@ const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
 let segundos = 0;
+let timer;
 
 function cronometro() {
     const data = new Date(segundos * 1000);
     return data.toLocaleTimeString('pt-BR', {hour12: false, timeZone: 'GMT'});
-
 }
 
 iniciar.addEventListener('click', function iniciarCronometro() {
-    const timer = setInterval(function() {
+    clearInterval(timer);
+    timer = setInterval(function() {
+        relogio.classList.remove('pausado');
         segundos++;
         relogio.innerHTML = cronometro(timer);
     }, 1000)
 });
 
+pausar.addEventListener('click', function pausei() {
+    clearInterval(timer);
+    relogio.classList.add('pausado');
+});
 
+zerar.addEventListener('click', function zerarTimer() {
+    clearInterval(timer);
+    relogio.classList.remove('pausado');
+    segundos = 0;
+    relogio.innerHTML = '00:00:00';
+});
