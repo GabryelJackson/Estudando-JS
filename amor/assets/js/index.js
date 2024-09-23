91b82212efcf013a5472220246d76c6155801cc2
GabryelJackson <gabryeljackson5@gmail.com>
2024-09-23 12:18:58 -0300

    codigo do amor

---
+++ amor/assets/js/index.js
@@ -0,0 +1,14 @@
+function sim () {
+    alert('Vamo pra cima minha vida <3');
+}
+
+function desvia() {
+    let botaoNao = document.querySelector('.nao');
+    botaoNao.style.position = 'absolute';
+    botaoNao.style.bottom = aleatorio(0, 100) + "%";
+    botaoNao.style.right = aleatorio(0, 100) + "%";
+} 
+
+function aleatorio(min, max) {
+    return Math.random() * (max - min) + min;
+}
